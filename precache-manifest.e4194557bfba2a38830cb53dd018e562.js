167f31f1f7b479bddb577b82b1b6964ab76fe216
dvshannon96@gmail.com <dvshannon96@gmail.com>
2019-06-26 17:22:10 -0400

    Updates

---
+++ precache-manifest.e4194557bfba2a38830cb53dd018e562.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af10bb3c9dff2bce7a71593cdcc3ebe5",
+    "url": "/remember-the-throne/index.html"
+  },
+  {
+    "revision": "d6609fcf266bba523dc6",
+    "url": "/remember-the-throne/static/css/main.d150dffb.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/remember-the-throne/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "d6609fcf266bba523dc6",
+    "url": "/remember-the-throne/static/js/main.218ac62a.chunk.js"
+  },
+  {
+    "revision": "598710effd75e85d995e",
+    "url": "/remember-the-throne/static/js/runtime~main.6d9f577d.js"
+  }
+]);
